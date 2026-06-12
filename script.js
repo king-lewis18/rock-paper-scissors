425ddeb41472f3fb035b185663a5c65350c3c8f5
king-lewis18 <king9lewis@gmail.com>
2026-06-12 13:35:59 +0300

    Add script.js

--- script.js
+++ script.js
@@ -14,6 +14,42 @@ function getHumanChoice() {
 }
 
 
+let humanScore = 0;
+let ComputerScore = 0;
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor (Math.random() * 3);
+    return choices[randomIndex];
+}
+
+
+function getHumanChoice() {
+    let choice = prompt("Choose rock, paper or scissors");
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+        return choice;
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log ("It's a tie!");
+    }else if (
+        humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "scissors" && computerChoice === "paper" 
+    ) {
+        console.log ("You Win!")
+        humanScore ++;
+    } else {
+        console.log ("You Lose!")
+        ComputerScore ++;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
 
 
 
