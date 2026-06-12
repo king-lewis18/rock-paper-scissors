7080d1e5e3b09f068eaf77940976b0a549f0a6be
king-lewis18 <king9lewis@gmail.com>
2026-06-12 10:53:34 +0300

    Edit code.html and script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor (Math.random() * 3);
+    return choices[randomIndex];
+}
+
+console.log(getComputerChoice());
